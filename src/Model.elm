88a5d17b1cf164eb6dc35c2f4efe98704b3c36c8
katjam <katjamordaunt@gmail.com>
2025-06-10 10:59:50 +0100

    Add image & quote stubs

--- src/Model.elm
+++ src/Model.elm
@@ -1,4 +1,4 @@
-module Model exposing (Model)
+module Model exposing (CaseStudy, Image, Model, Quote)
 
 import Browser.Navigation
 import Route exposing (Route)
@@ -7,4 +7,29 @@ import Route exposing (Route)
 type alias Model =
     { key : Browser.Navigation.Key
     , page : Route
+    }
+
+
+type alias Image =
+    { src : String
+    , alt : String
+    }
+
+
+type alias Quote =
+    { quote : String
+    , author : String
+    , source : String
+    }
+
+
+type alias CaseStudy =
+    { slug : String
+    , title : String
+    , introMarkdown : String
+    , maybeIntroImage : Maybe Image
+    , whatWeDidMarkdown : String
+    , maybeWhatWeDidImage : Maybe Image
+    , resultsMarkdown : String
+    , maybeQuote : Maybe Quote
     }
